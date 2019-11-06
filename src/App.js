02362f155b7644981e72b0f975f1d253584adb50
Filip <42967255+filipzakrocki@users.noreply.github.com>
2019-11-06 17:24:20 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <SearchBar />
+      <Results />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
