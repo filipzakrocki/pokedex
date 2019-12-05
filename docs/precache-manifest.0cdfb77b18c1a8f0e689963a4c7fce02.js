3ec0feaf57d0e30b55de72cd6cb1b8138da7fbd9
Filip <42967255+filipzakrocki@users.noreply.github.com>
2019-12-05 16:05:23 +0000

    test build deployment

---
+++ docs/precache-manifest.0cdfb77b18c1a8f0e689963a4c7fce02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058acccbf22f2125230ceb543b68ec2b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0bb0d012fcab1ef85852",
+    "url": "./static/css/main.77eb8ee3.chunk.css"
+  },
+  {
+    "revision": "6d2b932745f2deb5f7d9",
+    "url": "./static/js/2.92de9a82.chunk.js"
+  },
+  {
+    "revision": "0bb0d012fcab1ef85852",
+    "url": "./static/js/main.e4daeea5.chunk.js"
+  },
+  {
+    "revision": "a81e4a787e611e141577",
+    "url": "./static/js/runtime-main.e6fcf77b.js"
+  },
+  {
+    "revision": "7bbe31703b5c9387d36e95bee99ca7e3",
+    "url": "./static/media/logo.7bbe3170.png"
+  },
+  {
+    "revision": "f7b41b0b170d8765d7f6684497c7763a",
+    "url": "./static/media/pokeball.f7b41b0b.png"
+  }
+]);
