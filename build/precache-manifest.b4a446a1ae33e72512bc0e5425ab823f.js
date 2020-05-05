8cb3cc362e96399b5a2395fcdb81b5313b381d81
filipzakrocki <42967255+filipzakrocki@users.noreply.github.com>
2020-05-05 18:23:31 +0100

    new build

---
+++ build/precache-manifest.b4a446a1ae33e72512bc0e5425ab823f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c897bb6e14d8f4f44b46b0987c1029f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79fd999ef81eb4b85694",
+    "url": "/static/css/main.eb315eab.chunk.css"
+  },
+  {
+    "revision": "889216dbcadff800ceb1",
+    "url": "/static/js/2.e7f12aa7.chunk.js"
+  },
+  {
+    "revision": "79fd999ef81eb4b85694",
+    "url": "/static/js/main.55b4a5be.chunk.js"
+  },
+  {
+    "revision": "23e11197eb0ec2d8596f",
+    "url": "/static/js/runtime-main.d17f8506.js"
+  },
+  {
+    "revision": "7bbe31703b5c9387d36e95bee99ca7e3",
+    "url": "/static/media/logo.7bbe3170.png"
+  },
+  {
+    "revision": "f7b41b0b170d8765d7f6684497c7763a",
+    "url": "/static/media/pokeball.f7b41b0b.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.feb3a9674ae02f76fdd0a8d421f59c56.js"
+  "/precache-manifest.b4a446a1ae33e72512bc0e5425ab823f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
