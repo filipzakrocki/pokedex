6ef56435c0e75025c5d22383ca56832d547fe009
filipzakrocki <42967255+filipzakrocki@users.noreply.github.com>
2020-05-05 18:09:56 +0100

    new build

---
+++ build/precache-manifest.b726fd33e313f379b859a85144254a4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39bfac5331cfa8f64904907c407dab7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fdc573800a249c699129",
+    "url": "./static/css/main.eb315eab.chunk.css"
+  },
+  {
+    "revision": "beae4f8293749275f3d4",
+    "url": "./static/js/2.282243ac.chunk.js"
+  },
+  {
+    "revision": "fdc573800a249c699129",
+    "url": "./static/js/main.cf3f0ca3.chunk.js"
+  },
+  {
+    "revision": "a81e4a787e611e141577",
+    "url": "./static/js/runtime-main.e6fcf77b.js"
+  },
+  {
+    "revision": "7bbe31703b5c9387d36e95bee99ca7e3",
+    "url": "./static/media/logo.7bbe3170.png"
+  },
+  {
+    "revision": "f7b41b0b170d8765d7f6684497c7763a",
+    "url": "./static/media/pokeball.f7b41b0b.png"
+  }
+]);
